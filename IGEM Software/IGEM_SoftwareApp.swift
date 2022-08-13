2114ce8b9cb0edd9ba9cebdc0ab66b9f6572ed0e
Kyle Wang <kyle.sy.wang@gmail.com>
2022-08-13 11:43:32 +0800

    Initial Commit

---
+++ IGEM Software/IGEM_SoftwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  IGEM_SoftwareApp.swift
+//  IGEM Software
+//
+//  Created by Kyle Wang on 2022/8/13.
+//
+
+import SwiftUI
+
+@main
+struct IGEM_SoftwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
